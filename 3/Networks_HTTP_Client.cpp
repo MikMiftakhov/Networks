0556a286fa905895df53d7a06c9919a61d67cdf6
Mik_M <113305759+MikMiftakhov@users.noreply.github.com>
2022-12-13 19:14:05 +0300

    Add files via upload

---
+++ 3/Networks_HTTP_Client.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <windows.h>
+#include <string>
+
+#pragma comment (lib, "Ws2_32.lib")
+
+#define request "get/ http/1.1\r\n Host:127.0.0.1:1605\r\n\r\n"				//html запрос.
+
+#define SERV_PORT 1605
+#define max_packet_size   65535
+
+using namespace std;
+
+int main()
+{
+	int erStat;
+
+	WSADATA wsData;													//автоматически в момент создания загружаются данные о версии сокетов, используемых ОС, а также иная связанная системная информация
+																	//Функция WSAStartup инициирует использование библиотеки DLL сокетов Windows процессом
+	erStat = WSAStartup(0x0202, &wsData);							// в случае успеха возвращает 0, а в случае каких-то проблем возвращает код ошибки
+	if (erStat != 0)
+	{
+		cout << "Error WinSock version initializaion #" << WSAGetLastError() << endl;
+		return 1;
+	}
+	else
+		cout << "WinSock initialization is OK" << endl;
+
+
+	SOCKET ClientSock = socket(AF_INET, SOCK_STREAM, 0);				//создание и инициализация сокета
+	if (ClientSock == INVALID_SOCKET) {
+		cout << "Error initialization socket #" << WSAGetLastError() << endl;
+		closesocket(ClientSock);
+		WSACleanup();
+		return 1;
+	}
+	else
+		cout << "Client socket initialization is OK" << endl;
+
+	sockaddr_in servInfo;																				//структура  с доп информацией для сокета, например ip-адресом и портом
+
+	in_addr ip_to_num;
+	erStat = inet_pton(AF_INET, "127.0.0.1", &ip_to_num);												//inet_pton помещает в ip_to_num нужный адрес	//работаю через внутреннюю петлю, ибо с другими не работает) Нашему было абслоютно похуй, мб вашей нет
+	if (erStat <= 0) {
+		cout << "Error in IP translation to special numeric format" << endl;
+		return 1;
+	}
+
+	ZeroMemory(&servInfo, sizeof(servInfo));
+	servInfo.sin_family = AF_INET;
+	servInfo.sin_addr = ip_to_num;
+	servInfo.sin_port = htons(1605);
+
+	erStat = connect(ClientSock, (sockaddr*)&servInfo, sizeof(servInfo));								//привязываем доп инфу к сокету 
+	if (erStat != 0) {
+		cout << "Connection to Server is FAILED. Error # " << WSAGetLastError() << endl;
+		closesocket(ClientSock);
+		WSACleanup();
+		return 1;
+	}
+	else
+		cout << "Connection established SUCCESSFULLY. Ready to send a message to Server" << endl;
+
+		
+	erStat = send(ClientSock, (char*)&request, sizeof(request), 0);										//отправка запроса серверу
+	if (SOCKET_ERROR == erStat)
+	{
+		cout << "Send errorr # " << WSAGetLastError() << endl;
+		return -1;
+	}
+	else
+		cout << "Sending successful" << endl;
+
+	char buff[max_packet_size];
+	int len = 0;
+	do
+	{
+		len = recv(ClientSock, (char*)&buff, max_packet_size, 0);										//Получаем инфу
+		if (SOCKET_ERROR == len)
+		{
+			cout << "Receive error # " << WSAGetLastError() << endl;
+			return -1;
+		}
+		else
+			cout << "Receipt Successfully" << endl;
+
+		for (int i = 0; i < len; i++)
+			cout << buff[i];
+	} while (len != 0);																						//получаем данные по частям, пока не len != 0.
+	closesocket(ClientSock);
+	cin.get();
+	return 1;
+}
---
+++ 3/Networks_HTTP_Server.cpp
@@ -0,0 +1,132 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <windows.h>
+#include <sstream>
+#include <string>
+
+#pragma comment (lib, "Ws2_32.lib")
+
+using namespace std;
+
+int main()
+{
+	int erStat;																								
+	WSADATA wsData;											
+															
+	erStat = WSAStartup(0x0202, &wsData);					
+	if (erStat != 0)
+	{
+		cout << "Error WinSock version initializaion #" << WSAGetLastError() << endl;
+		return 1;
+	}
+	else
+		cout << "WinSock initialization is OK" << endl;
+
+	struct addrinfo* servInfo = NULL;												//более удобная структура для добавления информации сокету
+	struct addrinfo hints;															// структура для IP-адреса  сервера
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;	
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	erStat = getaddrinfo("127.0.0.1", "1605", &hints, &servInfo);					//не ебу как работаает, по идее тоже привязка к сокету
+	if (erStat != 0)
+	{
+		cout << "Getaddrinfo failed: " << erStat << "\n";
+		WSACleanup();
+	}
+	else
+		cout << "Getaddrinfo is OK" << endl;
+
+	int ServSock = socket(servInfo->ai_family, servInfo->ai_socktype, servInfo->ai_protocol);				//создание и инициализация сокета
+	if (ServSock == INVALID_SOCKET) {
+		cout << "Error initialization socket #" << WSAGetLastError() << endl;
+		closesocket(ServSock);
+		WSACleanup();
+		return 1;
+	}
+	else
+		cout << "Server socket initialization is OK" << endl;
+
+	erStat = bind(ServSock, servInfo->ai_addr, (int)servInfo->ai_addrlen);						//привязываем доп инфу к сокету 
+	if (erStat != 0) {
+		cout << "Error Socket binding to server info. Error #" << WSAGetLastError() << endl;
+		freeaddrinfo(servInfo);
+		closesocket(ServSock);
+		WSACleanup();
+		return 1;
+	}
+	else
+		cout << "Binding socket to Server info is OK" << endl;
+
+	erStat = listen(ServSock, 3);
+	if (erStat != 0) {
+		cout << "Can't start to listen to. Error # " << WSAGetLastError() << endl;
+		closesocket(ServSock);
+		WSACleanup();
+		return 1;
+	}
+	else
+		cout << "Listening..." << endl;
+
+	const int max_client_buffer_size = 1024;
+	char buf[max_client_buffer_size];
+	int ClientSock = INVALID_SOCKET;
+
+	while (1)
+	{
+		ClientSock = accept(ServSock, NULL, NULL);
+		if (ClientSock == INVALID_SOCKET)
+		{
+			cout << "Accept failed: " << WSAGetLastError() << endl;
+			closesocket(ServSock);
+			WSACleanup();
+			return 1;
+		}
+		else
+			cout << "Accept" << endl;
+
+		erStat = recv(ClientSock, buf, max_client_buffer_size, 0);
+		std::stringstream response;
+		std::stringstream response_body;
+		if (erStat == SOCKET_ERROR)
+		{
+			cout << "Recv failed: " << erStat << endl;
+			closesocket(ClientSock);
+		}
+		else if (erStat == 0)
+			cout << "Connection closed..."  << endl;
+		else if (erStat > 0)
+		{
+			buf[erStat] = '\0';
+			response_body << "<title>Test C++ HTTP Server</title>\n"			// формируем тело ответа (HTML)
+				<< "<h1>Test page</h1>\n"
+				<< "<p>This is body of the test page...</p>\n"
+				<< "<h2>Request headers</h2>\n"
+				<< "<pre>" << buf << "</pre>\n"
+				<< "<em><small>Test C++ Http Server</small></em>\n";
+
+			// Формируем весь ответ вместе с заголовками
+			response << "HTTP/1.1 200 OK\r\n" << "Version: HTTP/1.1\r\n" << "Content-Type: text/html; charset=utf-8\r\n" << "Content-Length: " << response_body.str().length()
+				<< "\r\n\r\n" << response_body.str();
+
+			// Отправляем ответ клиенту 
+			erStat = send(ClientSock, response.str().c_str(),
+					response.str().length(), 0);
+			if (erStat == SOCKET_ERROR)
+				cerr << "Send failed:" << WSAGetLastError() << endl; // произошла ошибка при отправке данных
+			else
+				cout << "Send"<< endl;
+			// Закрываем соединение к клиентом
+			closesocket(ClientSock);
+		}
+	}
+	closesocket(ServSock);
+	freeaddrinfo(servInfo);
+	WSACleanup();
+	return 0;
+}
+
+//дальше либо через браузер открываем 127.0.0.1:1605, либо через telnet   open 127.0.0.1 1605
